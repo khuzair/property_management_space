b4983b03c6a736c2d305f89abbcfadafc5be4a85
khuzair <affan4211@gmail.com>
2023-01-17 14:46:18 +0530

    some addition

--- api/views.py
+++ api/views.py
@@ -12,16 +12,23 @@ class PropertyAPI(APIView):
         serializer = PropertyModelSerializer(queryset, many=True)
         city_name = self.request.query_params.get('city_name', None)
         state_name = self.request.query_params.get('state_name', None)
+        property_id = self.request.query_params.get('property_id', None)
         
         
         if city_name is not None:
             queryset = queryset.filter(city=city_name)
             serializer = PropertyModelSerializer(queryset, many=True)
 
+        if property_id is not None:
+            data = queryset.values_list('city', flat=True).get(id=property_id)
+            print(str(data))
+            queryset = queryset.filter(city=data)
+            print(queryset)
+            serializer = PropertyModelSerializer(queryset, many=True)
+
         if state_name is not None:
             queryset = queryset.filter(state=state_name)
             serializer = CarListPropertyModelsSerializer(queryset, many=True)
-
 
         return Response(serializer.data)
 
